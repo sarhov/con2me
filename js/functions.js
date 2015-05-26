ba77567521ff1bee05df15f1ce8d1b6fca7d636c
sarhov <sarhov@sarhov.com>
2015-05-27 00:47:33 +0400

    canvas menu

--- js/functions.js
+++ js/functions.js
@@ -3,4 +3,5 @@ console.log('I\'m ready');
 $('.jsOpenNav').click(function(event) {
   event.preventDefault();
   $('.jsCanvas').toggleClass('is-open');
-});
+  $('body').toggleClass('overflow')
+}); 
