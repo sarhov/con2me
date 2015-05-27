d84b3a251537721cb50e6fd88096b769e5fc9909
sarhov <sarhov@sarhov.com>
2015-05-27 12:07:03 +0400

    menu checkboxes

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,16 @@ $('.jsOpenNav').click(function(event) {
   $('.jsBar').toggleClass('is-active');
   $('body').toggleClass('offMenu');
 }); 
+
+$('.jsMenuItem').click(function(event) {
+  event.preventDefault();
+  $(this).toggleClass('is-checked');
+  if ($(this).hasClass('jsMenuItemMain')) {
+    if ($(this).hasClass('is-checked')) {
+      $('.jsMenuItem').addClass('is-checked');
+    }
+    else{
+      $('.jsMenuItem').removeClass('is-checked');
+    }
+  };
+});
