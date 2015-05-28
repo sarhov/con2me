a56e677539923c657e8511119dfb5f903ca2fd2c
sarhov <sarhov@sarhov.com>
2015-05-28 12:39:51 +0400

    added add product section

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,13 @@ $('.jsMenuItem').click(function(event) {
     }
   };
 });
+
+document.getElementById("customButton").addEventListener("click", function(){
+  document.getElementById("fileUpload").click();  // trigger the click of actual file upload button
+});
+
+document.getElementById("fileUpload").addEventListener("change", function(){
+  var fullPath = document.getElementById('fileUpload').value;
+  var fileName = fullPath.split(/(\\|\/)/g).pop();  // fetch the file name
+  document.getElementById("fileName").innerHTML = fileName;  // display the file name
+}, false);
